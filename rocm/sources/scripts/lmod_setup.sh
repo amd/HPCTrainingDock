452ebc37b1694fbbe8b7ac9b2fc00a543c640ca4
Bob Robey <Bob.Robey@amd.com>
2024-07-21 20:43:23 -0500

    Adding comments to cupy install and some handling of other distros

--- rocm/sources/scripts/lmod_setup.sh
+++ rocm/sources/scripts/lmod_setup.sh
@@ -7,7 +7,7 @@ echo ""
 if [ "${DISTRO}" = "ubuntu" ]; then
    sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update
 fi
-if [ "${DISTRO}" = "ubuntu" ]; then
+if [ "${DISTRO}" = "rocky linux" ]; then
    sudo yum -y install lmod
 fi
 
