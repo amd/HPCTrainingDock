c516ac90a944c156761b51a0d07f19a2aa981958
Bob Robey <Bob.Robey@amd.com>
2024-08-05 17:03:51 -0500

    Fixes for ROCm 6.2.0

--- rocm/sources/scripts/rocm_setup.sh
+++ rocm/sources/scripts/rocm_setup.sh
@@ -146,6 +146,21 @@ ubuntu-set()
                    ;;
            esac
            ;;
+       6.2*)
+           case "${DISTRO_VERSION}" in
+               24.04)
+                   ROCM_REPO_DIST="noble"
+                   ;;
+               22.04)
+                   ROCM_REPO_DIST="jammy"
+                   ;;
+               20.04)
+                   ROCM_REPO_DIST="focal"
+                   ;;
+               *)
+                   ;;
+           esac
+           ;;
        *)
            ;;
    esac
@@ -323,7 +338,11 @@ if [ "${DISTRO}" == "ubuntu" ]; then
 
          # Run the amdgpu-install script. We have already installed the kernel driver, so use we use --no-dkms
          sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y ./amdgpu-install_${AMDGPU_INSTALL_VERSION}_all.deb
-         DEBIAN_FRONTEND=noninteractive amdgpu-install -q -y --usecase=hiplibsdk,rocm --no-dkms
+	 if [[ "${ROCM_VERSION}" -lt "6.2.0" ]]; then
+            DEBIAN_FRONTEND=noninteractive amdgpu-install -q -y --usecase=hiplibsdk,rocm --no-dkms
+	 else
+            DEBIAN_FRONTEND=noninteractive amdgpu-install -q -y --usecase=hiplibsdk,rocmdev,openmpsdk,mlsdk,asan --no-dkms
+	 fi
 
          # Required by DeepSpeed
          sudo ln -s /opt/rocm-${ROCM_VERSION}/.info/version /opt/rocm-${ROCM_VERSION}/.info/version-dev
@@ -464,3 +483,72 @@ cat <<-EOF | sudo tee ${MODULE_PATH}/${ROCM_VERSION}.lua
 	prepend_path("PATH", pathJoin(base, "bin"))
 	family("OpenCL")
 EOF
+
+if [[ "${ROCM_VERSION}" -ge "6.2.0" ]]; then
+   sudo mkdir -p /opt/rocm-${ROCM_VERSION}/omnitrace/
+   # The - option suppresses tabs
+   cat <<-EOF | sudo tee /opt/rocm-${ROCM_VERSION}/omnitrace/6.2.0.lua
+	whatis("Name: omnitrace")
+	whatis("Version: 6.2.0")
+	whatis("Category: AMD")
+	whatis("omnitrace")
+
+	local base = "/opt/rocm-${ROCM_VERSION}"
+
+	prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+	prepend_path("C_INCLUDE_PATH", pathJoin(base, "include"))
+	prepend_path("CPLUS_INCLUDE_PATH", pathJoin(base, "include"))
+	prepend_path("CPATH", pathJoin(base, "include"))
+	prepend_path("PATH", pathJoin(base, "bin"))
+	prepend_path("INCLUDE", pathJoin(base, "include"))
+	setenv("OMNITRACE_PATH", base)
+	load("rocm/${ROCM_VERSION}")
+	setenv("ROCP_METRICS", pathJoin(os.getenv("ROCM_PATH"), "/lib/rocprofiler/metrics.xml"))
+EOF
+
+   sudo mkdir -p /opt/rocm-${ROCM_VERSION}/omniperf/
+   # The - option suppresses tabs
+   cat <<-EOF | sudo tee /opt/rocm-${ROCM_VERSION}/omniperf/6.2.0.lua
+	local help_message = [[
+
+	Omniperf is an open-source performance analysis tool for profiling
+	machine learning/HPC workloads running on AMD MI GPUs.
+
+	Version 6.2.0
+	]]
+
+	help(help_message,"\n")
+
+	whatis("Name: omniperf")
+	whatis("Version: 2.0.0")
+	whatis("Keywords: Profiling, Performance, GPU")
+	whatis("Description: tool for GPU performance profiling")
+	whatis("URL: https://github.com/AMDResearch/omniperf")
+
+	-- Export environmental variables
+	local topDir="/opt/rocm-${ROCM_VERSION}"
+	local binDir="/opt/rocm-${ROCM_VERSION}/bin"
+	local shareDir="/opt/rocmplus-${ROCM_VERSION}/share"
+	local pythonDeps="/opt/rocmplus-${ROCM_VERSION}/python-libs"
+	local roofline="/opt/rocmplus-${ROCM_VERSION}/bin/utils/rooflines/roofline-ubuntu20_04-mi200-rocm5"
+
+	setenv("OMNIPERF_DIR",topDir)
+	setenv("OMNIPERF_BIN",binDir)
+	setenv("OMNIPERF_SHARE",shareDir)
+	setenv("ROOFLINE_BIN",roofline)
+
+	-- Update relevant PATH variables
+	prepend_path("PATH",binDir)
+	if ( pythonDeps  ~= "" ) then
+	prepend_path("PYTHONPATH",pythonDeps)
+	end
+
+	-- Site-specific additions
+	-- depends_on "python"
+	-- depends_on "rocm"
+	prereq(atleast("rocm","${ROCM_VERSION}"))
+	--  prereq("mongodb-tools")
+	local home = os.getenv("HOME")
+	setenv("MPLCONFIGDIR",pathJoin(home,".matplotlib"))
+EOF
+fi
