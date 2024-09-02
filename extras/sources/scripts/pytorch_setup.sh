e87b02f002cf7a25771d8223fcbc853a2e4a2ba1
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-09-02 15:27:07 -0500

    Added if statement on $USER for previous commit

--- extras/sources/scripts/pytorch_setup.sh
+++ extras/sources/scripts/pytorch_setup.sh
@@ -129,8 +129,10 @@ else
       done
 
       cd pytorch
-      sed -i '266i os.environ["ROCM_HOME"] = '"${ROCM_HOME}"'' setup.py
-      sed -i '266i os.environ["PYTORCH_ROCM_ARCH"] = '"${PYTORCH_ROCM_ARCH}"'' setup.py
+      if [[ "${USER}" == "root" ]]; then
+         sed -i '266i os.environ["ROCM_HOME"] = '"${ROCM_HOME}"'' setup.py
+         sed -i '266i os.environ["PYTORCH_ROCM_ARCH"] = '"${PYTORCH_ROCM_ARCH}"'' setup.py
+      fi
       # Pytorch 2.4 needs some patches to build for ROCm
       # NOT NEEDED ANYMORE:Fix triton build failure due to tritonlang.blob.core.windows.net not available
       # The download from https://tritonlang.blob.core.windows.net/llvm-builds/ has been
