45d73971eb3fa3f4bb6195bad62d2b3f412affaf
Giacomo Capodaglio <Giacomo.Capodaglio@amd.com>
2025-01-08 23:24:00 -0600

    Fixing a typo for flang-new module

--- extras/scripts/flang-new_setup.sh
+++ extras/scripts/flang-new_setup.sh
@@ -8,7 +8,7 @@ ROCM_VERSION=6.0
 DISTRO=`cat /etc/os-release | grep '^NAME' | sed -e 's/NAME="//' -e 's/"$//' | tr '[:upper:]' '[:lower:]' `
 DISTRO_VERSION=`cat /etc/os-release | grep '^VERSION_ID' | sed -e 's/VERSION_ID="//' -e 's/"$//' | tr '[:upper:]' '[:lower:]' `
 ARCHIVE_NAME="rocm-afar-6711-drop-5.1.0"
-ARCHIVE_DIR="rocm-afar-6711-drop.5.1.0"
+ARCHIVE_DIR="rocm-afar-6711-drop-5.1.0"
 
 SUDO="sudo"
 
