8af28b8f25eeee07978e31c14a14f3e46242e7da
Giacomo Capodaglio <Giacomo.Capodaglio@amd.com>
2025-02-25 16:40:42 -0600

    Modified hipfort script

--- extras/scripts/hipfort_setup.sh
+++ extras/scripts/hipfort_setup.sh
@@ -4,7 +4,9 @@
 MODULE_PATH=/etc/lmod/modules/ROCmPlus-LatestCompilers/hipfort
 BUILD_HIPFORT=0
 ROCM_VERSION=6.0.0
-USE_FLANGNEW=0
+HIPFORT_PATH="/opt/rocmplus-${ROCM_VERSION}/hipfort"
+HIPFORT_PATH_INPUT=""
+FC_COMPILER=gfortran
 
 SUDO="sudo"
 
@@ -18,7 +20,8 @@ usage()
    echo "  --module-path [ MODULE_PATH ] default $MODULE_PATH"
    echo "  --rocm-version [ ROCM_VERSION ] default $ROCM_VERSION"
    echo "  --build-hipfort [ BUILD_HIPFORT ], set to 1 to build hipfort, default is $BUILD_HIPFORT"
-   echo "  --use-flang-new [ USE_FLANGNEW ], , default is $USE_FLANGNEW"
+   echo "  --fc-compiler [FC_COMPILER: gfortran|amdflang-new|cray-ftn], default is $FC_COMPILER"
+   echo "  --install-path [ HIPFORT_PATH ], default is $HIPFORT_PATH"
    echo "  --help: this usage information"
    exit 1
 }
@@ -52,9 +55,14 @@ do
           MODULE_PATH=${1}
           reset-last
           ;;
-      "--use-flang-new")
+      "--install-path")
           shift
-          USE_FLANGNEW=${1}
+          HIPFORT_PATH_INPUT=${1}
+          reset-last
+          ;;
+      "--fc-compiler")
+          shift
+          FC_COMPILER=${1}
           reset-last
           ;;
       "--rocm-version")
@@ -73,12 +81,21 @@ do
    shift
 done
 
+if [ "${HIPFORT_PATH_INPUT}" != "" ]; then
+   HIPFORT_PATH=${HIPFORT_PATH_INPUT}
+else
+   # override path in case ROCM_VERSION has been supplied as input
+   HIPFORT_PATH=/opt/rocmplus-${ROCM_VERSION}/hipfort
+fi
+
 echo ""
 echo "==================================="
 echo "Starting Hipfort Install with"
 echo "ROCM_VERSION: $ROCM_VERSION"
 echo "BUILD_HIPFORT: $BUILD_HIPFORT"
-echo "USE_FLANGNEW: $USE_FLANGNEW"
+echo "MODULE_PATH: $MODULE_PATH"
+echo "HIPFORT_PATH: $HIPFORT_PATH"
+echo "FC_COMPILER: $FC_COMPILER"
 echo "==================================="
 echo ""
 
@@ -86,7 +103,7 @@ if [ "${BUILD_HIPFORT}" = "0" ]; then
 
    echo "Hipfort will not be built, according to the specified value of BUILD_HIPFORT"
    echo "BUILD_HIPFORT: $BUILD_HIPFORT"
-   exit 
+   exit
 
 else
    if [ -f /opt/rocmplus-${ROCM_VERSION}/CacheFiles/hipfort.tgz ]; then
@@ -109,6 +126,8 @@ else
       echo "============================"
       echo ""
 
+      if  [ "${BUILD_HIPFORT}" = "0" ]; then
+
       source /etc/profile.d/lmod.sh
       source /etc/profile.d/z01_lmod.sh
       module load rocm/${ROCM_VERSION}
@@ -122,34 +141,40 @@ else
       mkdir build
       cd build
 
-      if [ "${USE_FLANGNEW}" = "1" ]; then
+      if [ "${FC_COMPILER}" = "gfortran" ]; then
+         cmake -DHIPFORT_INSTALL_DIR=${HIPFORT_PATH} ..
+      elif [ "${FC_COMPILER}" = "amdflang-new" ]; then
          module load amdflang-new-beta-drop
          cmake -DHIPFORT_INSTALL_DIR=${HIPFORT_PATH} -DHIPFORT_COMPILER=$FC -DHIPFORT_COMPILER_FLAGS="-ffree-form -cpp" ..
+      elif [ "${FC_COM{ILER}" = "cray-ftn" ]; then
+         cmake -DHIPFORT_INSTALL_DIR=$HIPFORT_PATH -DHIPFORT_BUILD_TYPE=RELEASE -DHIPFORT_COMPILER=$(which ftn) -DHIPFORT_COMPILER_FLAGS="-ffree -eT" -DHIPFORT_AR=$(which ar) -DHIPFORT_RANLIB=$(which ranlib) ..
       else
-         cmake -DHIPFORT_INSTALL_DIR=${HIPFORT_PATH} ..
+         echo " ERROR: requested compiler is not currently among the available options "
+         echo " Please choose one among: gfortran (default), amdflang-new, cray-ftn "
+         exit 1
       fi
-
-      # if compiler is cray ftn
-#      cmake -DHIPFORT_INSTALL_DIR=$HIPFORT_PATH -DHIPFORT_BUILD_TYPE=RELEASE -DHIPFORT_COMPILER=$(which ftn) -DHIPFORT_COMPILER_FLAGS="-ffree -eT" -DHIPFORT_AR=$(which ar) -DHIPFORT_RANLIB=$(which ranlib) ..
 
       ${SUDO} make install
 
       cd ../..
       ${SUDO} rm -rf hipfort
 
-      module unload rocm/${ROCM_VERSION}
-      if [ "${USE_FLANGNEW}" = "1" ]; then
-         module unload amdflang-new-beta-drop
-      fi
-
    fi
 
+   if [ ! -w ${MODULE_PATH} ]; then
+      SUDO="sudo"
+   fi
    # Create a module file for hipfort
    ${SUDO} mkdir -p ${MODULE_PATH}
 
    # The - option suppresses tabs
    cat <<-EOF | ${SUDO} tee ${MODULE_PATH}/${ROCM_VERSION}.lua
 	whatis(" hipfc: Wrapper to call Fortran compiler with hipfort. Also calls hipcc for non Fortran files. ")
+        whatis(" this hipfort build has been compiled with: $FC_COMPILER. ")
+        local fc-compiler = $FC_COMPILER
+        if fc-compiler == "amdflang-new" then
+		load("amdflang-new-beta-drop")
+        end
 	load("rocm/${ROCM_VERSION}")
 	prepend_path("PATH","${HIPFORT_PATH}/bin")
 EOF
