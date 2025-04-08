409781088d1990369d854a3f04401aec8cefb645
Bob Robey <Bob.Robey@amd.com>
2025-04-08 03:50:15 -0500

    Updating version of mpi4py to fix test hang

--- extras/scripts/compiler_setup.sh
+++ extras/scripts/compiler_setup.sh
@@ -24,8 +24,12 @@ GCC_BASE_VERSION=`ls /usr/bin/gcc-* | cut -f2 -d'-' | grep '^[[:digit:]]'`
 GCC_VERSION_LIST=`apt list |grep '^gcc-[[:digit:]]*\/' |cut -f2 -d'-' | cut -f1 -d'/' | sort -n | tr '\n' ' '`
 echo "GCC_BASE_VERSION is ${GCC_BASE_VERSION}, GCC_VERSION_LIST is ${GCC_VERSION_LIST}"
 
+${SUDO} ${DEB_FRONTEND} apt-get -qy install gcc-$GCC_BASE_VERSION-offload-amdgcn
+exit
+
 MODULE_PATH=/etc/lmod/modules/Linux/gcc
 ${SUDO} mkdir -p ${MODULE_PATH}
+
 
 val=80
 for GCC_VERSION in ${GCC_VERSION_LIST}
