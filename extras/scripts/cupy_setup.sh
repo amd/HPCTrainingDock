9bb9771b6eea90ed5c99d0ed19e2753433ed20b8
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2025-04-15 10:11:51 -0500

    Installing full xarray for cupy

--- extras/scripts/cupy_setup.sh
+++ extras/scripts/cupy_setup.sh
@@ -157,7 +157,7 @@ else
       if [[ "${USER}" != "root" ]]; then
          ${SUDO} chmod a+w /opt/rocmplus-${ROCM_VERSION}/cupy
       fi
-      pip3 install -v --target=/opt/rocmplus-${ROCM_VERSION}/cupy pytest mock xarray dask
+      pip3 install -v --target=/opt/rocmplus-${ROCM_VERSION}/cupy pytest mock xarray[complete] dask
       pip3 install -v --upgrade --target=/opt/rocmplus-${ROCM_VERSION}/cupy dist/*.whl
       pip3 install -v --target=/opt/rocmplus-${ROCM_VERSION}/cupy cupy-xarray --no-deps
       if [[ "${USER}" != "root" ]]; then
