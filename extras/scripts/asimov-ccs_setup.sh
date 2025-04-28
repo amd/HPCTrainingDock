1589c36e8ac7da597cacd597af1ddd69d08bdd46
Bob Robey <Bob.Robey@amd.com>
2025-04-28 18:56:33 -0500

    Adding module for asimov

---
+++ extras/scripts/asimov-ccs_setup.sh
@@ -0,0 +1,117 @@
+#/bin/bash
+
+# Variables controlling setup process
+AMDGPU_GFXMODEL=`rocminfo | grep gfx | sed -e 's/Name://' | head -1 |sed 's/ //g'`
+MODULE_PATH=/etc/lmod/modules/misc/asimov_ccs
+BUILD_ASIMOV_CCS=1
+ROCM_VERSION=6.4.0
+SUDO="sudo"
+DEB_FRONTEND="DEBIAN_FRONTEND=noninteractive"
+
+if [  -f /.singularity.d/Singularity ]; then
+   SUDO=""
+   DEB_FRONTEND=""
+fi
+
+# Autodetect defaults
+DISTRO=`cat /etc/os-release | grep '^NAME' | sed -e 's/NAME="//' -e 's/"$//' | tr '[:upper:]' '[:lower:]' `
+DISTRO_VERSION=`cat /etc/os-release | grep '^VERSION_ID' | sed -e 's/VERSION_ID="//' -e 's/"$//' | tr '[:upper:]' '[:lower:]' `
+
+usage()
+{
+   echo "Usage:"
+   echo "  WARNING: when specifying --install-path and --module-path, the directories have to already exist because the script checks for write permissions"
+   echo "  WARNING: when selecting the module to supply to --mpi-module, make sure it sets the MPI_PATH environment variable"
+   echo "  --module-path [ MODULE_PATH ] default $MODULE_PATH"
+   echo "  --rocm-version [ ROCM_VERSION ] default $ROCM_VERSION"
+   echo "  --mpi-module [ MPI_MODULE ] default $MPI_MODULE"
+   echo "  --amdgpu-gfxmodel [ AMDGPU-GFXMODEL ] default autodetected"
+   echo "  --build-asimov-ccs [ BUILD_ASIMOV_CCS ] default is 0"
+   echo "  --help: this usage information"
+   exit 1
+}
+
+send-error()
+{
+    usage
+    echo -e "\nError: ${@}"
+    exit 1
+}
+
+reset-last()
+{
+   last() { send-error "Unsupported argument :: ${1}"; }
+}
+
+n=0
+while [[ $# -gt 0 ]]
+do
+   case "${1}" in
+      "--amdgpu-gfxmodel")
+          shift
+          AMDGPU_GFXMODEL=${1}
+          reset-last
+          ;;
+      "--build-asimov-ccs")
+          shift
+          BUILD_ASIMOV_CCS=${1}
+          reset-last
+          ;;
+      "--help")
+          usage
+          ;;
+      "--module-path")
+          shift
+          MODULE_PATH=${1}
+          reset-last
+          ;;
+      "--rocm-version")
+          shift
+          ROCM_VERSION=${1}
+          reset-last
+          ;;
+      "--*")
+          send-error "Unsupported argument at position $((${n} + 1)) :: ${1}"
+          ;;
+      *)
+         last ${1}
+         ;;
+   esac
+   n=$((${n} + 1))
+   shift
+done
+
+echo ""
+echo "==================================="
+echo "Starting ASiMoV Module Install"
+echo "==================================="
+echo ""
+
+if [ "${BUILD_ASIMOV_CCS}" = "0" ]; then
+
+   echo "ASIMOV_CCS will not be built, according to the specified value of BUILD_ASIMOV_CCS"
+   echo "BUILD_ASIMOV_CCS: $BUILD_ASIMOV_CCS"
+   exit
+
+else
+
+   ${SUDO} mkdir -p ${MODULE_PATH}
+
+
+   # The - option suppresses tabs
+   cat <<-EOF | ${SUDO} tee ${MODULE_PATH}/dev.lua
+        whatis("ASiMoV-CCS package")
+
+        load("adios2")
+        load("fyaml")
+        load("hdf5")
+        load("makedepf90")
+        load("makedepf90")
+        load("parhip")
+        load("parmetis")
+        load("petsc")
+        load("pyyaml_lit")
+        load("rcm")
+EOF
+
+fi
