faec36e3f45b586ea42c9f65aaef56f585f62faa
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2025-05-14 17:42:05 -0500

    Fixes to aomp script

--- extras/scripts/aomp_setup.sh
+++ extras/scripts/aomp_setup.sh
@@ -4,6 +4,8 @@
 export AOMP_VERSION_NUMBER=19.0-3
 export AOMP_VERSION_SHORT=19.0
 export MODULE_PATH=/etc/lmod/modules/ROCmPlus-LatestCompilers/aomp
+export AOMP_PATH=/opt/rocmplus-${ROCM_VERSION}
+AOMP_PATH_INPUT=""
 
 SUDO="sudo"
 DEB_FRONTEND="DEBIAN_FRONTEND=noninteractive"
@@ -25,6 +27,7 @@ usage()
    echo "  --amdgpu-gfxmodel [ AMDGPU_GFXMODEL ] default autodetected"
    echo "  --module-path [ MODULE_PATH ] default /etc/lmod/modules/ROCmPlus-LatestCompilers/aomp"
    echo "  --rocm-version [ ROCM_VERSION ] default $ROCM_VERSION"
+   echo "  --install-path [ AOMP_PATH ] default ${AOMP}/aomp_${AOMP_VERSION_NUMBER}"
    echo "  --help: this usage information"
    exit 1
 }
@@ -63,6 +66,11 @@ do
           MODULE_PATH=${1}
           reset-last
           ;;
+      "--install-path")
+          shift
+          AOMP_PATH_INPUT=${1}
+          reset-last
+          ;;
       "--rocm-version")
           shift
           ROCM_VERSION=${1}
@@ -79,12 +87,20 @@ do
    shift
 done
 
+if [ "${AOMP_PATH_INPUT}" != "" ]; then
+   AOMP_PATH=${AOMP_PATH_INPUT}
+else
+   # override path in case ROCM_VERSION has been supplied as input
+   AOMP_PATH=/opt/rocmplus-${ROCM_VERSION}
+fi
 
 echo ""
 echo "==================================="
 echo "Starting AOMP Latest Install with"
 echo "ROCM_VERSION: $ROCM_VERSION"
 echo "BUILD_AOMP_LATEST: $BUILD_AOMP_LATEST"
+echo "INSTALL_PATH: $AOMP_PATH/aomp_${AOMP_VERSION_NUMBER}"
+echo "MODULE_PATH: $MODULE_PATH"
 echo "AOMP_VERSION_NUMBER: $AOMP_VERSION_NUMBER"
 echo "==================================="
 echo ""
@@ -115,14 +131,19 @@ if [ "${BUILD_AOMP_LATEST}" = "1" ]; then
       echo "What directory for AOMP build?"
       pwd
       echo "====================================="
-      export AOMP=/opt/rocmplus-${ROCM_VERSION}/aomp
-      if [[ "${USER}" != "root" ]]; then
-         ${SUDO} chmod a+rwX /opt
-      fi
+
+      ${SUDO} mkdir -p $AOMP_PATH
+      mkdir -p $AOMP_PATH
 
 # Installs aomp from .deb package but then we can't specify where to install it
 #     wget -q https://github.com/ROCm/aomp/releases/download/rel_19.0-0/aomp_Ubuntu2204_19.0-0_amd64.deb
 #     apt-get install ./aomp_Ubuntu2204_19.0-0_amd64.deb
+
+      # installing in $HOME then moving to $AOMP_PATH
+      cd $HOME
+      mkdir aomp_tmp_install_dir
+      cd aomp_tmp_install_dir
+      export AOMP=$PWD/aomp
 
       ${SUDO} apt-get update
       ${SUDO} ${DEB_FRONTEND} apt-get install -y gawk ninja-build generate-ninja ccache libssl-dev \
@@ -141,21 +162,36 @@ if [ "${BUILD_AOMP_LATEST}" = "1" ]; then
       cd ..
       rm -rf aomp-${AOMP_VERSION_NUMBER}.tar.gz aomp${AOMP_VERSION_SHORT}
 
-      if [[ "${USER}" != "root" ]]; then
-         ${SUDO} find /opt -type f -execdir chown root:root "{}" +
-         ${SUDO} chmod go-w /opt
-      fi
+      # move installation from $HOME dir to desired AOMP_PATH
+      cd $AOMP_PATH
+      ${SUDO} mv ${AOMP}_${AOMP_VERSION_NUMBER} $PWD
+      mv ${AOMP}_${AOMP_VERSION_NUMBER} $PWD
+      cd aomp*
+      export AOMP_PATH=$PWD
 
    fi
 
-   # In either case, create a module file for AOMP compiler
+   # Create a module file for aomp
+   if [ -d "$MODULE_PATH" ]; then
+      # use sudo if user does not have write access to module path
+      if [ ! -w ${MODULE_PATH} ]; then
+         SUDO="sudo"
+      else
+         echo "WARNING: not using sudo since user has write access to module path"
+      fi
+   else
+      # if module path dir does not exist yet, the check on write access will fail
+      SUDO="sudo"
+      echo "WARNING: using sudo, make sure you have sudo privileges"
+   fi
+
    ${SUDO} mkdir -p ${MODULE_PATH}
 
    # The - option suppresses tabs
    cat <<-EOF | ${SUDO} tee ${MODULE_PATH}/amdclang-${AOMP_VERSION_SHORT}.lua
-	whatis("AMD OpenMP Compiler version 19.0-3 based on LLVM")
+	whatis("AMD OpenMP Compiler version ${AOMP_VERSION_NUMBER} based on LLVM")
 
-	local base = "/opt/rocmplus-${ROCM_VERSION}/aomp_19.0-3"
+	local base = "${AOMP_PATH}"
 
 	prepend_path("PATH", pathJoin(base, "bin"))
 	setenv("CC", pathJoin(base, "bin/amdclang"))
