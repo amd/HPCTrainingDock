445f8a27bb22c7fbf6168eac86f35ac5a3f9aff5
Bob Robey <Bob.Robey@amd.com>
2025-08-05 16:08:04 -0500

    Removing lib64 from rocm module since it is not used

--- rocm/scripts/rocm_setup.sh
+++ rocm/scripts/rocm_setup.sh
@@ -426,7 +426,6 @@ cat <<-EOF | ${SUDO} tee ${MODULE_PATH}/${ROCM_VERSION}.lua
 	local mbase = " /etc/lmod/modules/ROCm/rocm"
 
 	prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
-	prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib64"))
 	prepend_path("C_INCLUDE_PATH", pathJoin(base, "include"))
 	prepend_path("CPLUS_INCLUDE_PATH", pathJoin(base, "include"))
 	prepend_path("CPATH", pathJoin(base, "include"))
