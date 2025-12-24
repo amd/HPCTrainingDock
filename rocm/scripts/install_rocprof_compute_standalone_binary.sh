e640518c7cb26ef025144e4febe94b795a983ec0
Bob Robey <Bob.Robey@amd.com>
2025-12-23 18:19:40 -0600

    Fixing standaline binary installation

--- rocm/scripts/install_rocprof_compute_standalone_binary.sh
+++ rocm/scripts/install_rocprof_compute_standalone_binary.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export ROCM_VERSION=`cat ${ROCM_PATH}/.info/version | cut -f1 -d'-' `
-#export ROCM_VERSION=7.1.0
 if [[ "$ROCM_VERSION" == "" ]]; then
    echo "ROCM module not loaded or ROCM_VERSION not found"
    exit
 else
    echo "ROCM_VERSION is $ROCM_VERSION"
 fi
+export LD_LIBRARY_PATH="standalonebinary/lib/python3.10/site-packages/kaleido/executable/lib:$LD_LIBRARY_PATH"
 rm -rf standalonebinary
 python3 -m venv standalonebinary
 source standalonebinary/bin/activate
@@ -22,17 +22,18 @@ git checkout rocm-${ROCM_VERSION}
 #git checkout develop
 cd projects/rocprofiler-compute
 python3 -m pip install -r requirements.txt
+python3 -m pip install nuitka patchelf
 cmake -B build -DCMAKE_INSTALL_PREFIX=install -S .
 cmake --build build --target install --parallel 8
+make -C build standalonebinary
+cmake --build build --target standalonebinary
 
 echo ""
-echo ""
-echo ""
-echo ""
-find . -name rocprof-compute* -print
+find . -name rocprof-compute.bin -print
 
 sudo mv $ROCM_PATH/bin/rocprof-compute $ROCM_PATH/bin/rocprof-compute.back
-sudo cp install/bin/rocprof-compute $ROCM_PATH/bin/rocprof-compute
+sudo cp src/rocprof-compute.dist/rocprof-compute.bin $ROCM_PATH/bin/rocprof-compute.bin
+pushd $ROCM_PATH/bin && sudo ln -s rocprof-compute.bin rocprof-compute && popd
 
 deactivate
 cd ../../../..
