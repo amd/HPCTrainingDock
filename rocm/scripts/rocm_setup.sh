67e47775327547aa670222beb7cb8a598eb5938f
Bob Robey <Bob.Robey@amd.com>
2026-01-15 11:38:23 -0600

    Adding nuitka one file version or rocprof-compute to rocm install

--- rocm/scripts/rocm_setup.sh
+++ rocm/scripts/rocm_setup.sh
@@ -555,50 +555,53 @@ echo "DEBUG INCLUDE_TOOLS is ${INCLUDE_TOOLS}"
 
 if [ "${INCLUDE_TOOLS}" = "1" ]; then
 
-   TOOL_NAME=omnitrace
-   TOOL_EXEC_NAME=omnitrace
-   TOOL_NAME_MC=Omnitrace
-   TOOL_NAME_UC=OMNITRACE
-   # if ROCM_VERSION is greater than 6.2.9, the awk command will give the ROCM_VERSION number
-   result=`echo ${ROCM_VERSION} | awk '$1>6.2.9'` && echo $result
-   if [[ "${result}" ]]; then
-      TOOL_NAME=rocprofiler-systems
-      TOOL_EXEC_NAME=rocprof-sys-avail
-      TOOL_NAME_MC=Rocprofiler-systems
-      TOOL_NAME_UC=ROCPROFILER_SYSTEMS
-   fi
+   # if ROCM_VERSION is greater than equal to 7.1.0, the sort by version will give the smaller ROCM_VERSION number
+   if [ "$(printf '%s\n' "7.1.0" "$ROCM_VERSION" | sort -V | head -n1)" != "7.1.0" ]; then
 
-   echo ""
-   echo "=================================="
-   echo "Starting ROCm ${TOOL_NAME_MC} Install with"
-   echo "DISTRO: $DISTRO"
-   echo "DISTRO_VERSION: $DISTRO_VERSION"
-   echo "ROCM_VERSION: $ROCM_VERSION"
-   echo "AMDGPU_ROCM_VERSION: $AMDGPU_ROCM_VERSION"
-   echo "AMDGPU_INSTALL_VERSION: $AMDGPU_INSTALL_VERSION"
-   echo "=================================="
-   echo ""
-
-   # if ROCM_VERSION is greater than 6.1.2, the awk command will give the ROCM_VERSION number
-   # if ROCM_VERSION is less than or equal to 6.1.2, the awk command result will be blank
-   result=`echo $ROCM_VERSION | awk '$1>6.1.2'` && echo $result
-   if [[ "${result}" == "" ]]; then
-      echo "ROCm built-in ${TOOL_NAME_MC} version cannot be installed on ROCm versions before 6.2.0"
-      exit
-   fi
-   if [[ -f /opt/rocm-${ROCM_VERSION}/bin/${TOOL_EXEC_NAME} ]] ; then
-      echo "ROCm built-in ${TOOL_NAME_MC} already installed"
-   else
-      if [ "${DISTRO}" == "ubuntu" ]; then
-         ${SUDO} ${DEB_FRONTEND} apt-get install -q -y ${TOOL_NAME}
+      TOOL_NAME=omnitrace
+      TOOL_EXEC_NAME=omnitrace
+      TOOL_NAME_MC=Omnitrace
+      TOOL_NAME_UC=OMNITRACE
+      # if ROCM_VERSION is greater than 6.2.9, the awk command will give the ROCM_VERSION number
+      result=`echo ${ROCM_VERSION} | awk '$1>6.2.9'` && echo $result
+      if [[ "${result}" ]]; then
+         TOOL_NAME=rocprofiler-systems
+         TOOL_EXEC_NAME=rocprof-sys-avail
+         TOOL_NAME_MC=Rocprofiler-systems
+         TOOL_NAME_UC=ROCPROFILER_SYSTEMS
       fi
-   fi
 
-   if [[ -f /opt/rocm-${ROCM_VERSION}/bin/${TOOL_EXEC_NAME} ]] ; then
-      export MODULE_PATH=/etc/lmod/modules/ROCm/${TOOL_NAME}
-      ${SUDO} mkdir -p ${MODULE_PATH}
-      # The - option suppresses tabs
-   cat <<-EOF | ${SUDO} tee ${MODULE_PATH}/${ROCM_VERSION}.lua
+      echo ""
+      echo "=================================="
+      echo "Starting ROCm ${TOOL_NAME_MC} Install with"
+      echo "DISTRO: $DISTRO"
+      echo "DISTRO_VERSION: $DISTRO_VERSION"
+      echo "ROCM_VERSION: $ROCM_VERSION"
+      echo "AMDGPU_ROCM_VERSION: $AMDGPU_ROCM_VERSION"
+      echo "AMDGPU_INSTALL_VERSION: $AMDGPU_INSTALL_VERSION"
+      echo "=================================="
+      echo ""
+
+      # if ROCM_VERSION is greater than 6.1.2, the awk command will give the ROCM_VERSION number
+      # if ROCM_VERSION is less than or equal to 6.1.2, the awk command result will be blank
+      result=`echo $ROCM_VERSION | awk '$1>6.1.2'` && echo $result
+      if [[ "${result}" == "" ]]; then
+         echo "ROCm built-in ${TOOL_NAME_MC} version cannot be installed on ROCm versions before 6.2.0"
+         exit
+      fi
+      if [[ -f /opt/rocm-${ROCM_VERSION}/bin/${TOOL_EXEC_NAME} ]] ; then
+         echo "ROCm built-in ${TOOL_NAME_MC} already installed"
+      else
+         if [ "${DISTRO}" == "ubuntu" ]; then
+            ${SUDO} ${DEB_FRONTEND} apt-get install -q -y ${TOOL_NAME}
+         fi
+      fi
+
+      if [[ -f /opt/rocm-${ROCM_VERSION}/bin/${TOOL_EXEC_NAME} ]] ; then
+         export MODULE_PATH=/etc/lmod/modules/ROCm/${TOOL_NAME}
+         ${SUDO} mkdir -p ${MODULE_PATH}
+         # The - option suppresses tabs
+      cat <<-EOF | ${SUDO} tee ${MODULE_PATH}/${ROCM_VERSION}.lua
 	whatis("Name: ${TOOL_NAME}")
 	whatis("Version: ${ROCM_VERSION}")
 	whatis("Category: AMD")
@@ -621,6 +624,7 @@ if [ "${INCLUDE_TOOLS}" = "1" ]; then
         set_shell_function("omnitrace-run",'/opt/rocm-${ROCM_VERSION}/bin/rocprof-sys-run "$@"',"/opt/rocm-${ROCM_VERSION}/bin/rocprof-sys-run $*")
 EOF
 
+      fi
    fi
 
    TOOL_NAME=omniperf
@@ -645,76 +649,221 @@ EOF
    echo "=================================="
    echo ""
 
-   # if ROCM_VERSION is greater than 6.1.2, the awk command will give the ROCM_VERSION number
-   # if ROCM_VERSION is less than or equal to 6.1.2, the awk command result will be blank
-   result=`echo $ROCM_VERSION | awk '$1>6.1.2'` && echo $result
-   if [[ "${result}" == "" ]]; then
-      echo "ROCm built-in ${TOOL_NAME_MC} version cannot be installed on ROCm versions before 6.2.0"
-      exit
-   fi
-   if [[ -f /opt/rocm-${ROCM_VERSION}/bin/${TOOL_EXEC_NAME} ]] ; then
-      echo "ROCm built-in ${TOOL_NAME_MC} already installed"
-   else
-      if [ "${DISTRO}" == "ubuntu" ]; then
-         ${SUDO} ${DEB_FRONTEND} apt-get install -q -y ${TOOL_NAME}
+   # if ROCM_VERSION is greater than equal to 7.1.0, the sort by version will give the smaller ROCM_VERSION number
+   if [ "$(printf '%s\n' "7.1.0" "$ROCM_VERSION" | sort -V | head -n1)" = "7.1.0" ]; then
+      echo "This is where we put the nuitka installation"
+      ROCM_PATH=$INSTALL_PATH
+      PATH="${ROCM_PATH}:${PATH}"
+
+      #rm -rf standalonebinary
+      python3 -m venv standalonebinary
+      source standalonebinary/bin/activate
+      cd standalonebinary
+      git clone --no-checkout --filter=blob:none https://github.com/ROCm/rocm-systems.git
+      cd rocm-systems
+      git sparse-checkout init --cone
+      git sparse-checkout set projects/rocprofiler-compute
+      git branch --list
+      git checkout rocm-${ROCM_VERSION}
+      #git checkout develop
+      cd projects/rocprofiler-compute
+      mv requirements.txt requirements.txt.back
+
+      cat <<-EOF > requirements.txt
+	astunparse==1.6.2
+	blinker==1.9.0
+	certifi==2026.1.4
+	charset-normalizer==3.4.4
+	click==8.3.1
+	colorlover==0.3.0
+	contourpy==1.3.2
+	cycler==0.12.1
+	dash==3.3.0
+	dash-bootstrap-components==2.0.4
+	dash-svg==0.0.12
+	dnspython==2.8.0
+	Flask==3.1.2
+	fonttools==4.61.1
+	greenlet==3.3.0
+	idna==3.11
+	importlib_metadata==8.7.1
+	itsdangerous==2.2.0
+	Jinja2==3.1.6
+	kaleido==0.2.1
+	kiwisolver==1.4.9
+	linkify-it-py==2.0.3
+	markdown-it-py==4.0.0
+	MarkupSafe==3.0.3
+	matplotlib==3.10.8
+	mdit-py-plugins==0.5.0
+	mdurl==0.1.2
+	narwhals==2.15.0
+	nest-asyncio==1.6.0
+	Nuitka==2.6
+	numpy==2.2.6
+	ordered-set==4.1.0
+	packaging==25.0
+	pandas==2.3.3
+	patchelf==0.17.2.4
+	pillow==12.1.0
+	platformdirs==4.5.1
+	plotext==5.3.2
+	plotille==5.0.0
+	plotly==6.5.1
+	Pygments==2.19.2
+	pymongo==4.16.0
+	pyparsing==3.3.1
+	python-dateutil==2.9.0
+	pytz==2025.2
+	PyYAML==6.0.3
+	requests==2.32.5
+	retrying==1.4.2
+	rich==14.2.0
+	six==1.17.0
+	SQLAlchemy==2.0.45
+	tabulate==0.9.0
+	textual==7.0.1
+	textual-fspicker==0.6.0
+	textual-plotext==1.0.1
+	tqdm==4.67.1
+	typing_extensions==4.15.0
+	tzdata==2025.3
+	uc-micro-py==1.0.3
+	urllib3==2.6.3
+	Werkzeug==3.1.5
+	zipp==3.23.0
+	zstandard==0.25.0
+EOF
+
+      python3 -m pip install -r requirements.txt
+      #python3 -m pip install nuitka==2.6 patchelf
+
+      # Using CMakeLists.txt
+      #cmake -B build -DCMAKE_INSTALL_PREFIX=$ROCM_PATH/bin -S .
+      #cmake --build build --target standalonebinary
+
+      # Local version from CMakeLists.txt example
+      # Change working directory to src
+      cd src
+      # Create VERSION.sha file
+      git -C .. rev-parse HEAD > VERSION.sha
+      # Build standalone binary
+      # NOTE: --no-deployment-flag=self-execution is used to avoid self-execution
+      # and fork bombs as explained in
+      # https://nuitka.net/user-documentation/common-issue-solutions.html#fork-bombs-self-execution
+      export PROJECT_SOURCE_DIR=`pwd`/..
+         python3 -m nuitka --mode=onefile --no-deployment-flag=self-execution \
+           --include-data-files=${PROJECT_SOURCE_DIR}/VERSION*=./ --enable-plugin=no-qt --enable-plugin=no-qt \
+           --include-package=dash_svg --include-package-data=dash_svg \
+           --include-package=dash_bootstrap_components \
+           --include-package-data=dash_bootstrap_components --include-package=plotly \
+           --include-package-data=plotly --noinclude-data-files=plotly/datasets/* --include-package=kaleido \
+           --include-package-data=kaleido --include-package=rocprof_compute_analyze \
+           --include-package-data=rocprof_compute_analyze \
+           --include-package=rocprof_compute_profile \
+           --include-package-data=rocprof_compute_profile \
+           --include-package=rocprof_compute_tui --include-package-data=rocprof_compute_tui \
+           --include-package=rocprof_compute_soc --include-package-data=rocprof_compute_soc \
+           --include-package=utils --include-package-data=utils rocprof-compute
+      # Remove library rpath from executable
+      patchelf --remove-rpath rocprof-compute.bin
+      # Move to build directory
+      echo "ROCM_PATH is ${ROCM_PATH}"
+      echo "sudo cp rocprof-compute.bin $ROCM_PATH/bin/rocprof-compute.bin"
+      sudo cp rocprof-compute.bin $ROCM_PATH/bin/rocprof-compute.bin
+      cd ..
+
+      src/rocprof-compute.bin --help
+      echo "return code is $?"
+
+      echo ""
+      find . -name rocprof-compute.bin -print
+
+      if [[ ! -L $ROCM_PATH/bin/rocprof-compute.py ]]; then
+         sudo mv $ROCM_PATH/bin/rocprof-compute $ROCM_PATH/bin/rocprof-compute.py
       fi
-   fi
-
-   if [[ "${USER}" != "root" ]]; then
-      ${SUDO} chmod -R a+w /opt/rocm-${ROCM_VERSION}
-   fi
-
-   PYTHON=python3
-   if [ "${PYTHON_VERSION}" != "" ]; then
-      PYTHON=python3.${PYTHON_VERSION}
-   fi
-
-   ${PYTHON} -m pip install -t /opt/rocm-${ROCM_VERSION}/libexec/${TOOL_NAME}/python-libs -r /opt/rocm-${ROCM_VERSION}/libexec/${TOOL_NAME}/requirements.txt
-
-   if [[ -f /opt/rocm-${ROCM_VERSION}/bin/${TOOL_EXEC_NAME} ]] ; then
-
-      python3 -m venv rocprof-compute-exec
-      source rocprof-compute-exec/bin/activate
-      pip install pyinstaller
-      pip install -r /opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/requirements.txt
-
-      if [[ -f /opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/VERSION.sha ]]; then
-         pyinstaller --onefile /opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/rocprof-compute \
-           --add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/utils:utils" \
-           --add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/VERSION:." \
-           --add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/VERSION.sha:." \
-           --distpath rocprof-compute-exec/dist
-      else
-         pyinstaller --onefile /opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/rocprof-compute \
-           --add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute:." \
-           --distpath rocprof-compute-exec/dist
-           #--add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/utils:utils" \
-           #--add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/rocprof_compute_soc:rocprof_compute_soc" \
+      if [[ ! -L $ROCM_PATH/bin/rocprof-compute ]]; then
+         pushd $ROCM_PATH/bin && sudo ln -s rocprof-compute.bin rocprof-compute && popd
       fi
 
-      ls -RC rocprof-compute-exec/dist/
-      rocprof-compute-exec/dist/rocprof-compute --version
-      ${SUDO} cp rocprof-compute-exec/dist/rocprof-compute /opt/rocm-${ROCM_VERSION}/bin/rocprof-compute.exe
-      #${SUDO} rm -f /opt/rocm-${ROCM_VERSION}/bin/rocprof-compute
-      #cd /opt/rocm-${ROCM_VERSION}/bin && ${SUDO} ln -s rocprof-compute.exe rocprof-compute && cd -
-      rocprof-compute --version
+      du -skh $ROCM_PATH/bin/rocprof-compute.bin
+
       deactivate
-      rm -rf rocprof-compute-exec
+      cd ../../../..
+      rm -rf standalonebinary
 
-      # Restore original version
-      #${SUDO} rm -f /opt/rocm-${ROCM_VERSION}/bin/rocprof-compute
-      #cd /opt/rocm-${ROCM_VERSION}/bin && ${SUDO} ln -s ../libexec/rocprofiler-compute rocprof-compute && cd -
-   fi
+   else
 
-   if [[ "${USER}" != "root" ]]; then
-      ${SUDO} chmod go-w /opt/rocm-${ROCM_VERSION}
-   fi
+      # if ROCM_VERSION is greater than 6.1.2, the awk command will give the ROCM_VERSION number
+      # if ROCM_VERSION is less than or equal to 6.1.2, the awk command result will be blank
+      result=`echo $ROCM_VERSION | awk '$1>6.1.2'` && echo $result
+      if [[ "${result}" == "" ]]; then
+         echo "ROCm built-in ${TOOL_NAME_MC} version cannot be installed on ROCm versions before 6.2.0"
+         exit
+      fi
+      if [[ -f /opt/rocm-${ROCM_VERSION}/bin/${TOOL_EXEC_NAME} ]] ; then
+         echo "ROCm built-in ${TOOL_NAME_MC} already installed"
+      else
+         if [ "${DISTRO}" == "ubuntu" ]; then
+            ${SUDO} ${DEB_FRONTEND} apt-get install -q -y ${TOOL_NAME}
+         fi
+      fi
 
-   if [[ -f /opt/rocm-${ROCM_VERSION}/bin/${TOOL_EXEC_NAME} ]] ; then
-      export MODULE_PATH=/etc/lmod/modules/ROCm/${TOOL_NAME}
-      ${SUDO} mkdir -p ${MODULE_PATH}
-      # The - option suppresses tabs
-   cat <<-EOF | ${SUDO} tee ${MODULE_PATH}/${ROCM_VERSION}.lua
+      if [[ "${USER}" != "root" ]]; then
+         ${SUDO} chmod -R a+w /opt/rocm-${ROCM_VERSION}
+      fi
+
+      PYTHON=python3
+      if [ "${PYTHON_VERSION}" != "" ]; then
+         PYTHON=python3.${PYTHON_VERSION}
+      fi
+
+      ${PYTHON} -m pip install -t /opt/rocm-${ROCM_VERSION}/libexec/${TOOL_NAME}/python-libs -r /opt/rocm-${ROCM_VERSION}/libexec/${TOOL_NAME}/requirements.txt
+
+      if [[ -f /opt/rocm-${ROCM_VERSION}/bin/${TOOL_EXEC_NAME} ]] ; then
+
+         python3 -m venv rocprof-compute-exec
+         source rocprof-compute-exec/bin/activate
+         pip install pyinstaller
+         pip install -r /opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/requirements.txt
+
+         if [[ -f /opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/VERSION.sha ]]; then
+            pyinstaller --onefile /opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/rocprof-compute \
+              --add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/utils:utils" \
+              --add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/VERSION:." \
+              --add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/VERSION.sha:." \
+              --distpath rocprof-compute-exec/dist
+         else
+            pyinstaller --onefile /opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/rocprof-compute \
+              --add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute:." \
+              --distpath rocprof-compute-exec/dist
+              #--add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/utils:utils" \
+              #--add-data "/opt/rocm-${ROCM_VERSION}/libexec/rocprofiler-compute/rocprof_compute_soc:rocprof_compute_soc" \
+         fi
+
+         ls -RC rocprof-compute-exec/dist/
+         rocprof-compute-exec/dist/rocprof-compute --version
+         ${SUDO} cp rocprof-compute-exec/dist/rocprof-compute /opt/rocm-${ROCM_VERSION}/bin/rocprof-compute.exe
+         #${SUDO} rm -f /opt/rocm-${ROCM_VERSION}/bin/rocprof-compute
+         #cd /opt/rocm-${ROCM_VERSION}/bin && ${SUDO} ln -s rocprof-compute.exe rocprof-compute && cd -
+         rocprof-compute --version
+         deactivate
+         rm -rf rocprof-compute-exec
+
+         # Restore original version
+         #${SUDO} rm -f /opt/rocm-${ROCM_VERSION}/bin/rocprof-compute
+         #cd /opt/rocm-${ROCM_VERSION}/bin && ${SUDO} ln -s ../libexec/rocprofiler-compute rocprof-compute && cd -
+      fi
+
+      if [[ "${USER}" != "root" ]]; then
+         ${SUDO} chmod go-w /opt/rocm-${ROCM_VERSION}
+      fi
+
+      if [[ -f /opt/rocm-${ROCM_VERSION}/bin/${TOOL_EXEC_NAME} ]] ; then
+         export MODULE_PATH=/etc/lmod/modules/ROCm/${TOOL_NAME}
+         ${SUDO} mkdir -p ${MODULE_PATH}
+         # The - option suppresses tabs
+      cat <<-EOF | ${SUDO} tee ${MODULE_PATH}/${ROCM_VERSION}.lua
 	local help_message = [[
 
 	${TOOL_NAME_MC} is an open-source performance analysis tool for profiling
@@ -759,6 +908,6 @@ EOF
 	set_shell_function("omniperf",'/opt/rocm-${ROCM_VERSION}/bin/rocprof-compute "$@"',"/opt/rocm-${ROCM_VERSION}/bin/rocprof-compute $*")
 
 EOF
-
+      fi
    fi
 fi
