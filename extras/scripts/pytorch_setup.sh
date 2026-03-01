642ff26b2eb600e84c55cb4fea68b195f5084ef1
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2026-03-01 10:53:22 -0600

    Modified PyTorch module

--- extras/scripts/pytorch_setup.sh
+++ extras/scripts/pytorch_setup.sh
@@ -717,8 +717,9 @@ cat <<-EOF | ${SUDO} tee ${MODULE_PATH}/${PYTORCH_VERSION}.lua
 	prepend_path("PYTHONPATH","${TRITON_PATH}")
 
 	prepend_path("PATH","${PYTORCH_PATH}/bin")
-	setenv("MIOPEN_USER_DB_PATH","/tmp/$USER/my-miopen-cache")
-	setenv("MIOPEN_CUSTOM_CACHE_DIR","/tmp/$USER/my-miopen-cache")
+	local user = os.getenv("USER")
+	setenv("MIOPEN_USER_DB_PATH", "/tmp/" .. user .. "/my-miopen-cache")
+	setenv("MIOPEN_CUSTOM_CACHE_DIR", "/tmp/" .. user .. "/my-miopen-cache")
 	setenv("Torch_DIR","${PYTORCH_PATH}/lib/python3.${PYTHON_VERSION}/site-packages")
 EOF
 # An alternate module with tunable gemms
